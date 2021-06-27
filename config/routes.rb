910e9c186442c7a03ebebc58eaab69bbe837a65b
Mehmet <mehmetkusken@gmail.com>
2021-06-26 18:24:24 -0600

    commit tree eb6105c00a458216194cf062d5cd609a4b98cc64

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+RailsBlog::Application.routes.draw do
+  resources :posts
+  resources :tags
+  resources :users
+  resources :post_tags
 end
